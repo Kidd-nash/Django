1e8e4990f2fb0093a6bfaa620df1344fa1f438bb
Kidd-nash <arkady.pantaleon@outlook.com>
2025-06-05 14:38:00 +0800

    copied a simple python code from another repo, keynote changes first set the pounds or result to none, second request method post for input, third returning a render and lastly the except ValueError and TypeError for a simple data input validation

--- myapp/views.py
+++ myapp/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    pounds = None
+
+    if request.method == "POST":
+        try:
+            kilogram = float(request.POST.get("kilogram"))
+
+            pounds = f"{kilogram}kg is equal to {kilogram * 2.20462}lb/s converted."
+        except (ValueError, TypeError):
+            pounds = "You have given an invalid value."
+
+    return render(request, 'index.html', {'result': pounds})
