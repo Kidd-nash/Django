ecece2070d1dd14157e5ff74d87c68e6f98102ff
Kidd-nash <arkady.pantaleon@outlook.com>
2025-06-05 17:09:40 +0800

    GET syntax changed Get to GET, DO REMEMBER SYNTAX

--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    conversion_type = request.Get.get("convert")
+    conversion_type = request.GET.get("convert")
     result = None
 
     if request.method == "POST":
